d7e537dc91aaffd302cb5ea5ba01b62cead6995a
Tony Hsiao <tonycar12002@gmail.com>
2019-02-26 22:27:19 +0800

    move husky gazebo to other repo

---
+++ environment.sh
@@ -0,0 +1,6 @@
+source /opt/ros/kinetic/setup.bash
+source $HOME/husky_gazebo/catkin_ws/devel/setup.bash
+
+GAZEBO_MODEL_PATH=$HOME/husky_gazebo/catkin_ws/src/husky/husky_gazebo/models
+export GAZEBO_MODEL_PATH
+echo "Set GAZEBO_MODEL_PATH to: $GAZEBO_MODEL_PATH"
